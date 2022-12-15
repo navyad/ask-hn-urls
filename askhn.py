317385481e7743f49447c071c94e0dc4ded5e9d2
navyad <navyad.pro@gmail.com>
2022-12-15 10:22:17 +0530

    fetching page

--- askhn.py
+++ askhn.py
@@ -1,12 +1,19 @@
 import argparse
 from urllib.parse import urlparse
 
+import requests
+
+SCHEMES = ["https", "http"]
+BASE_URL = "news.ycombinator.com"
+PATH = "/item"
+CONTENT_TYPE = 'text/html; charset=utf-8'
+
 
 def is_ask_hn_url(url: str) -> bool:
     parse_result = urlparse(url)
-    is_scheme = parse_result.scheme in ["https", "http"]
-    is_netloc = parse_result.netloc == "news.ycombinator.com"
-    is_path = parse_result.path == "/item"
+    is_scheme = parse_result.scheme in SCHEMES
+    is_netloc = parse_result.netloc == BASE_URL
+    is_path = parse_result.path == PATH
     try:
         _ref, _post_id = parse_result.query.split("id=")
     except ValueError:
@@ -16,13 +23,28 @@ def is_ask_hn_url(url: str) -> bool:
     return all([is_scheme, is_netloc, is_path, is_post_id])
 
 
+def fetch_post(url: str) -> str:
+    response = requests.get(url=url)
+    if not response.status_code == 200:
+        print(f"request to {url} failed")
+        return
+    assert response.headers['Content-Type'] == CONTENT_TYPE
+    return response.text
+
+
+def scrap_post(url: str):
+    pass
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='Ask:HN post urls')
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('--url', type=str, help='url of post')
     args = parser.parse_args()
+    url = args.url
 
-    if not is_ask_hn_url(url=args.url):
-        print("It is not Ask:HN url")
+    if not is_ask_hn_url(url=url):
+        print(f"{url}: Not a Ask:HN url")
     else:
-        pass
+        print(f"fetching {url}")
+        page_source = fetch_post(url=url)
